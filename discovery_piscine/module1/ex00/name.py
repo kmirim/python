7fa67c230faa27b9dbb195b2160ba2a2a5c8a83a
kmirim <kmirim@DESC-NB-2368.>
2025-05-05 22:08:12 -0300

    second exercice

---
+++ discovery_piscine/module1/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Kaluana"
+last_name = "Barcelos"
+
+print(f"{first_name} {last_name}")
---
+++ discovery_piscine/module1/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Kaluana"
+last_name = "Barcelos"
+whole_name = "%s %s" % (first_name, last_name)
+print(whole_name)
